abf021d5af6c9bcd045f4958508be78f2da928a6
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2026-02-02 19:42:06 -0300

    feat: setup inicial do projeto Byte-Backend

---
+++ Dados/ByteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+// using Byte_Backend.Entidades; // Comentado porque ainda não existe
+
+namespace Byte_Backend.Dados;
+
+public class ByteDbContext : DbContext
+{
+    public ByteDbContext(DbContextOptions<ByteDbContext> options) : base(options) { }
+    
+    // public DbSet<Cargo> Cargos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
